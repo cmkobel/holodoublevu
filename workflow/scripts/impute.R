6a24f5ce1ba51b5750beb5c5c8e3106c0bee6bdc
Carl M. Kobel <carl.mathias.kobel@nmbu.no>
2024-08-12 23:39:44 +0200

    good first day, goodnight

--- workflow/scripts/impute.R
+++ workflow/scripts/impute.R
@@ -12,7 +12,7 @@ metadata <- read_tsv(snakemake@input[["metadata"]] %>% as.character())
 # metadata <- read_tsv("resources/metadata_v1.6.tsv")
 
 proteome_intensities_raw <- read_rds(snakemake@input[["filtered"]] %>% as.character())
-proteome_intensities_raw <- read_rds("results/filtered/proteome_intensities.rds")
+# proteome_intensities_raw <- read_rds("results/filtered/proteome_intensities.rds")
 
 samples <- snakemake@params[["samples"]]
 # samples <- c("D06T6S", "D08T6S", "D16T6S", "D25T6S", "D28T1S", "D28T2S", "D28T3S", "D28T4S", "D28T5S", "D28T6S", "D29T6S", "D35T6S", "D51T1S", "D51T2S", "D51T3S", "D51T4S", "D51T5S", "D51T6S", "D60T6S", "D61T6S", "D76T6S", "L06T6R", "L08T6R", "L16T6R", "L25T6R", "L28T6R", "L29T6R", "L35T6R", "L42T6R", "L51T6R", "L60T6R", "L61T6R", "L76T6R", "W06T6R", "W08T6R", "W16T6R", "W25T6R", "W29T6R", "W35T6R", "W42T6R", "W51T6R", "W60T6R", "W61T6R", "W76T6R") # luing example
@@ -108,3 +108,6 @@ imputed %>%
 
 imputed %>%
     write_rds(snakemake@output[["imputed"]] %>% as.character())
+
+imputation_group %>%
+    write_rds(snakemake@output[["imputation_group"]] as.character())
